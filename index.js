d0b6f368d17ca0764f540e8b0c55070402810ece
collins <kipkorirc583@gmail.com>
2023-06-25 11:39:07 +0300

    First sommit

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+
 function addingEventListener() {
-}
+    // Add event listener to the input element
+    const input = document.getElementById('input');
+    input.addEventListener('click', () => {
+      // Handle the click event
+    });
+  }
+  
+  module.exports = { addingEventListener };
+  
